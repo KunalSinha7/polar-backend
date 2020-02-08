9b37871fcec0f3c826e0eba6fcd0bb884215766b
Will Borland <will@wborland.com>
2020-02-08 13:50:30 -0500

    Some tests and user blueprint setup

--- flaskapp.py
+++ flaskapp.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, Blueprint, jsonify
+from users import user
 
 app = Flask(__name__)
-
+app.register_blueprint(user.user, url_prefix='/user')
 
 
 
@@ -13,9 +14,12 @@ def index():
 def test():
   return 'hello test'
 
-@app.route('/fout')
-def four():
-  return "Bad request", 401
+
+@app.route('/error', methods=['POST'])
+def error():
+  out = jsonify("Hello world")
+  return out, 401
+
 
 if __name__ == '__main__':
   app.run(threaded=True)
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -8,4 +8,3 @@ class BasicTestCase(BaseTestCase):
     def test_index(self):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertIn('Hello', str(response.data))
