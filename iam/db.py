26032fdb117b727b669b7c0446e06a6fdfc972f6
Darwin Vaz <dvaz@purdue.edu>
2020-02-20 14:46:46 -0500

    added tests for createRole

--- iam/db.py
+++ iam/db.py
@@ -21,10 +21,14 @@ def createRole(data):
 
     row = []
     for perm in data["permissions"]:
-        add_perm_cmd += ' (%s, %s),'
-        row.append(roleId)
-        row.append(perm)
-        
+        if perm > 0 and perm < 12:
+            add_perm_cmd += ' (%s, %s),'
+            row.append(roleId)
+            row.append(perm)
+
+    if len(row) == 0:
+        abort(400, "Specified permissions don't exists")
+
     add_perm_cmd = add_perm_cmd[:len(add_perm_cmd) - 1]
     add_perm_cmd += ';'
 
--- tests/__init__.py
+++ tests/__init__.py
@@ -53,8 +53,8 @@ class BaseTestCase(ut.TestCase):
 
     def fake_user(self):
         response = self.post('/user/register', dict(
-            first_name=rand_string(10),
-            last_name=rand_string(10),
+            firstName = rand_string(10),
+            lastName = rand_string(10),
             email = '{}@polarapp.com'.format(rand_string(5)),
             password = rand_string(20)
         ))
