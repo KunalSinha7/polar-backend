166425ea3150c6ed34de1c44773b70a3473b9b5b
Will Borland <will@wborland.com>
2020-02-22 21:15:40 -0500

    Looked at pylint

--- flaskapp.py
+++ flaskapp.py
@@ -1,12 +1,13 @@
-from flask import Flask, Blueprint, jsonify, make_response
+import os
+
+from flask import Flask, jsonify, make_response
 from user import user
 from iam import iam
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 
-import os
+
 import db
 import db.util
-import auth.jwt
 
 app = Flask(__name__)
 app.register_blueprint(user.user, url_prefix='/user')
@@ -14,11 +15,13 @@ app.register_blueprint(iam.iam, url_prefix='/iam')
 CORS(app, support_credentials=True)
 
 if os.environ.get('config') is None:
+    PATH = ''
     if os.path.isfile('/home/ubuntu/config.cfg'):
-        path = '/home/ubuntu/config.cfg'
+        PATH = '/home/ubuntu/config.cfg'
     else:
-        path = '../config.cfg'
+        PATH = '../config.cfg'
 
+    app.config.from_object(PATH)
 
 @app.cli.command("resetdb")
 def makedb():
@@ -29,7 +32,7 @@ def bad_request(error):
     return make_response(jsonify({'code': 400, 'message': error.description}), 400)
 
 @app.errorhandler(401)
-def unauthorized(error):
+def bad_auth(error):
     return make_response(jsonify({'code': 401, 'message': error.description}), 401)
 
 @app.errorhandler(403)
