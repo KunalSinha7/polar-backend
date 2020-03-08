5a72634ee910568dde2e198c2f7f643b81869fcb
Darwin Vaz <dvaz@purdue.edu>
2020-03-08 01:27:21 -0500

    uploading file to bucket

--- files/files.py
+++ files/files.py
@@ -6,6 +6,7 @@ import auth
 import auth.jwt
 import auth.perms
 import user.db as user
+import boto3
 
 files = Blueprint('files', __name__)
 
@@ -18,12 +19,41 @@ def upload():
     if 'name' not in data or 'desc' not in data or 'file' not in data or 'roles' not in data:
         abort(400, "Missing data")
     
-    # develop s3 module
+    # s3 = boto3.resource('s3')
+    bucket = "polar-files"
+
+    # file = ""
+    # s3.Bucket(bucket).upload_file(file, "xyz")
+
+
+    file_name = "C:\\Users\\Darwin Vaz\\Documents\\photo.jpg"
+    object_name = file_name
+    s3_client = boto3.client('s3')
+    response = s3_client.upload_file(file_name, bucket, "xyz")
+    print(response)
+
 
     data['store'] = data['name'] + '.txt'
     db.upload(data)
 
     return jsonify()
+
+
+@files.route('/download', methods=['POST'])
+@auth.login_required(perms=[2])
+def download():
+    # data = request.get_json()
+
+    s3 = boto3.resource('s3')
+    file_name = "xyz"
+    bucket = "polar-files"
+    # output = f"downloads/{file_name}"
+    output = file_name
+    s3.Bucket(bucket).download_file(file_name, output)
+    print(output)
+
+    return jsonify()
+    
 
 
 @files.route('/delete', methods=['POST'])
